91a8ea5cd1a26028f323d0a567ca1d547ab8b9b2
NexusSfan <131726447+NexusSfan@users.noreply.github.com>
2023-07-25 23:01:15 -0700

    1.1.1 DL test.
    fast version EOF.

--- pbskids-dl.sh
+++ pbskids-dl.sh
@@ -18,7 +18,7 @@ fi
 
 rawurl=($1)
 if [ "$1" == "--help" ]; then
-    echo "PBS Kids DL 1.1"
+    echo "PBS Kids DL 1.1.1"
     echo "A tool for downloading PBS Kids videos"
     echo
     echo "Put in a PBS Kids Video URL and it will download!"
@@ -31,7 +31,7 @@ vid_name=() # Name of the video
 realvid=() # Link to the raw video
 title=() # Name of the show
 
-# Fetch the titles and captions,
+# Fetch the titles and links,
 # and URLs that ffmpeg can use.
 # Store them in lists in memory!
 echo "Getting WebPage"
@@ -41,19 +41,17 @@ curl -s $rawurl | grep DEEPLINK
 echo "Getting Title"
 curl -s $rawurl | grep DEEPLINK | awk -F "," '{print $9}'
 echo "Decoding Title"
-curl -s $rawurl | grep DEEPLINK | awk -F "," '{print $9}' | awk -F "\"" '{print $4}'
-curl -s $rawurl | grep DEEPLINK | awk -F "," '{print $9}' | awk -F "\"" '{print $4}' | sed "s/[\]//g"
-curl -s $rawurl | grep DEEPLINK | awk -F "," '{print $9}' | awk -F "\"" '{print $4}' | sed "s/[\]//g" | sed "s/[/]//g"
 curl -s $rawurl | grep DEEPLINK | awk -F "," '{print $9}' | awk -F "\"" '{print $4}' | sed "s/[\]//g" | sed "s+/+\ -\ +g" |  sed "s/[\]//g"
 curl -s $rawurl | grep DEEPLINK | awk -F "," '{print $24}' | awk -F "\"" '{print $4}' | sed "s/[\]//g" | sed "s+/+-+g" |  sed "s/[\]//g"
 sleep 3
 vid_name=`curl -s $rawurl | grep DEEPLINK | awk -F "," '{print $9}' | awk -F "\"" '{print $4}' | sed "s/[\]//g" | sed "s+/+\ -\ +g" |  sed "s/[\]//g"`
 echo $vid_name
-realvid=`curl -s $rawurl | grep DEEPLINK | awk -F "," '{print $10}' | awk -F "\"" '{print $4}' | sed "s/[\]//g"`
-echo $captions
+realvid=`curl -s $rawurl | grep DEEPLINK | awk -F "," '{print $8}' | awk -F "\"" '{print $4}' | sed "s/[\]//g"`
+echo $realvid
 title=`curl -s $rawurl | grep DEEPLINK | awk -F "," '{print $24}' | awk -F "\"" '{print $4}' | sed "s/[\]//g" | sed "s+/+-+g" |  sed "s/[\]//g"`
 echo $title
-vid_title=`echo $title":"$vid_name | sed "s+\ +_+g" | sed "s+\.+_+g"`
+vid_title=`echo $title"_:_"$vid_name | sed "s+\ +_+g" | sed "s+\.+_+g"`
 echo $vid_title
 echo "Downloading Video..."
 ffmpeg -i "$realvid" ./$vid_title.mp4
+echo "The operation completed."
