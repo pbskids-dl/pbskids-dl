e4bf1dd5bf292799e6837d272c29769ab6a385a9
Kiavash <46830209+kiavash-at-work@users.noreply.github.com>
2024-05-31 14:49:41 -0700

    Functions and Python
    Practicing functions in python.
    
    Introducing main() a defined starting point for the execution of a program is useful for understanding how a program works.

--- pbskids-dl.py
+++ pbskids-dl.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+pbskids_dl_version = '3.0'
 
 try:
     import sys
@@ -11,53 +12,67 @@ except:
     sys.exit(128)
 
 def handle_progress(chunk_number, chunk_size, total_size):
-    total_chunk = total_size / chunk_size
     length = 50
-    decimals = 1
+    
+    total_chunk = total_size / chunk_size
     prefix = 'Downloading:'
-    suffix = ''
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (chunk_number / float(total_chunk)))
+    percent = ("{0:." + '1' + "f}").format(100 * (chunk_number / float(total_chunk)))
     filledLength = int(length * chunk_number // total_chunk)
     bar = '█' * filledLength + ' ' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = "\r")
+    print(f'\r{prefix} |{bar}| {percent}%', end = "\r")
     if chunk_number == total_chunk: 
         print()
 
+def cli_builder():
+    parser = argparse.ArgumentParser(prog='pbskids-dl', description='A tool for downloading PBS KIDS videos.', epilog='Made by NexusSfan')
+    parser.add_argument('url', help='The page you land on when a video is playing.')
+    parser.add_argument('-v', '--version', action='version', version='PBSKIDS DL '+pbskids_dl_version)
+    args = parser.parse_args()
+    return args
 
-parser = argparse.ArgumentParser(prog='pbskids-dl', description='A tool for downloading PBS KIDS videos.', epilog='Made by NexusSfan')
-parser.add_argument('url', help='The page you land on when a video is playing.')
-parser.add_argument('-v','--version', action='version', version='PBSKIDS DL 3.0')
-args = parser.parse_args()
+def fetch_script(url):
+    try:
+        response = urllib.request.urlopen(url)
+        webContent = response.read().decode('UTF-8')
+        soup = BeautifulSoup(webContent, features="lxml")
+        script = soup.find('script', type='application/json').text
+    except:
+        print('ERROR: The \"' + url + '\" link failed to load properly. Is it a PBS Kids Video link?', file=sys.stderr)
+        sys.exit(128)
+    return script
 
-try:
-    response = urllib.request.urlopen(args.url)
-    webContent = response.read().decode('UTF-8')
-    soup = BeautifulSoup(webContent, features="lxml")
-    script = soup.find('script', type='application/json').text
-except:
-    print('ERROR: The \"' + args.url + '\" link failed to load properly. Is it a PBS Kids Video link?', file=sys.stderr)
-    sys.exit(128)
+def find_assets(script):
+    try:
+        data = json.loads(script)
+        assets = data['props']['pageProps']['videoData']['mediaManagerAsset']
+        videos = assets['videos']
+    except:
+        print('ERROR: The video was not found! Is the link a PBS Kids Video link?', file=sys.stderr)
+        sys.exit(128)
+    return assets,videos
 
-try:
-    data = json.loads(script)
-    assets = data['props']['pageProps']['videoData']['mediaManagerAsset']
-    videos = assets['videos']
-except:
-    print('ERROR: The video was not found! Is the link a PBS Kids Video link?', file=sys.stderr)
-    sys.exit(128)
+def download_video(vid_title, video):
+    try:
+        realvid = video['url']
+        print('Downloading Video...')
+        print(realvid)
+        urllib.request.urlretrieve(realvid, vid_title, handle_progress)
+    except:
+        print('ERROR: The video cannot be downloaded! Might be a network issue.', file=sys.stderr)
+        sys.exit(128)
 
-vid_title = assets['title'].replace('/','+').replace('\\','+') + '.mp4'
-print(vid_title)
-for video in videos:
-    if (video['profile'] == 'mp4-16x9-baseline'):
-        try:
-            realvid = video['url']
-            print('Downloading Video...')
-            print(realvid)
-            urllib.request.urlretrieve(realvid, vid_title, handle_progress)
-        except:
-            print('ERROR: The video cannot be downloaded! Might be a network issue.', file=sys.stderr)
-            sys.exit(128)
-        break
-print("\nThe operation completed.")
-sys.exit(0)
+def main():
+    args = cli_builder()
+    script = fetch_script(args.url)
+    assets, videos = find_assets(script)
+    vid_title = assets['title'].replace('/','+').replace('\\','+') + '.mp4'
+    print(vid_title)
+    for video in videos:
+        if (video['profile'] == 'mp4-16x9-baseline'):
+            download_video(vid_title, video)
+            break
+    print("\nThe operation completed.")
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
