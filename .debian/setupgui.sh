f754dbc73eda3fab7317faadc2b62eadb0ccc6c2
NexusSfan <nexussfan@duck.com>
2024-06-01 18:15:16 -0700

    Remove debug commands to cut down on bloat

--- .debian/setupgui.sh
+++ .debian/setupgui.sh
@@ -3,19 +3,16 @@ pwd
 chmod +x *
 mkdir pbskids-dl_gui_deb
 cd ./pbskids-dl_gui_deb
-ls -al
 mkdir -p opt/pbskids-dl_gui
 cd ./opt/pbskids-dl_gui
 cp $GITHUB_WORKSPACE/pbskids-dl_gui.py .
 pyinstaller pbskids-dl_gui.py
-ls -al
 chmod +x ./dist/pbskids-dl_gui/pbskids-dl_gui
 cd ../..
 mkdir -p usr/bin
 cd ./usr/bin
 ln -s /opt/pbskids-dl_gui/dist/pbskids-dl_gui/pbskids-dl_gui pbskids-dl_gui
 cp $GITHUB_WORKSPACE/pbskids-dl_gui.sh .
-ls -al
 cd ../../
 mkdir DEBIAN
 cd ./DEBIAN
