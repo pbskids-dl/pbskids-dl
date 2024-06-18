c1520caa332211466425716dce507bfda7d59fff
NexusSfan <nexussfan@duck.com>
2024-06-18 01:19:57 +0000

    Update setupgui.sh

--- .debian/setupgui.sh
+++ .debian/setupgui.sh
@@ -1,8 +1,8 @@
 cd $GITHUB_WORKSPACE
 pwd
 chmod +x *
-mkdir pbskids-dl_deb
-cd ./pbskids-dl_deb
+mkdir pbskids-dl_gui_deb
+cd ./pbskids-dl_gui_deb
 mkdir -p usr/bin
 cd ./usr/bin
 cp $GITHUB_WORKSPACE/pbskids-dl_gui.py .
@@ -12,6 +12,5 @@ mkdir DEBIAN
 cd ./DEBIAN
 cp $GITHUB_WORKSPACE/.debian/control2 ./control
 cd $GITHUB_WORKSPACE
-dpkg --build ./pbskids-dl_deb
-sha256sum pbskids-dl_deb.deb > pbskids-dl.sha256sum
-sha512sum pbskids-dl_deb.deb > pbskids-dl.sha512sum
+dpkg --build ./pbskids-dl_gui_deb
+sha256sum pbskids-dl_gui_deb.deb > pbskids-dl_gui.sha256sum
