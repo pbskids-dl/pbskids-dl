0c3b283d7d7036d5b8b54146e43886668c3fe0a7
NexusSfan <nexussfan@duck.com>
2024-06-23 17:29:15 +0000

    Update .prcheck.sh

--- .prcheck.sh
+++ .prcheck.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 # Retrieve the list of checks
-checks=$(gh pr checks "$1" --watch --json)
-
-# Filter out the "dependabot" workflow
-filtered_checks=$(echo "$checks" | jq -r '.[] | select(.name != "dependabot")')
-
-# Print the filtered list of checks
-echo "$filtered_checks"
+gh pr checks "$1" --watch --json name,workflow | jq 'map(select(.workflow != "dependabot"))'
