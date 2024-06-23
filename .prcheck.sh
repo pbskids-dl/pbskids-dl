7b1b3a015b6d08676f892523e7c603217e06a71a
NexusSfan <nexussfan@duck.com>
2024-06-23 17:25:50 +0000

    Update .prcheck.sh

--- .prcheck.sh
+++ .prcheck.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Retrieve the list of checks
-checks=$(gh pr checks "$1" --json --watch)
+checks=$(gh pr checks "$1" --watch --json)
 
-# Filter out the "name2" workflow
-filtered_checks=$(echo "$checks" | jq -r '.[] | select(.name != "name2")')
+# Filter out the "dependabot" workflow
+filtered_checks=$(echo "$checks" | jq -r '.[] | select(.name != "dependabot")')
 
 # Print the filtered list of checks
 echo "$filtered_checks"
