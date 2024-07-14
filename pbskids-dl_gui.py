9fe53c9e2d9b89de550f89762ecf5859d33f0674
NexusSfan <nexussfan@duck.com>
2024-07-14 03:05:07 +0000

    Update pbskids-dl_gui.py

--- pbskids-dl_gui.py
+++ pbskids-dl_gui.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #    pbskids-dl
 #    Copyright (C) 2024 The pbskids-dl Team
 #
@@ -15,7 +16,6 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-#!/usr/bin/env python3
 pbskids_dl_version = '3.1'
 import sys
 
